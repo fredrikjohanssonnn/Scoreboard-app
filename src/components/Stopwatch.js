b11c6752e9e8b910d77ce0d2f335b6222e5fc2cf
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2020-07-27 15:51:02 +0200

    Added timer functionality

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -3,23 +3,48 @@ import React, { Component } from 'react';
 class StopWatch extends Component {
   state = {
     isRunning: false,
+    elapsedTime: 0,
+    previousTime: 0,
+  };
+
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.tick(), 100);
+  }
+
+  tick = () => {
+    if (this.state.isRunning) {
+      const now = Date.now();
+      this.setState((prevState) => ({
+        previousTime: now,
+        elapsedTime: prevState.elapsedTime + (now - this.state.previousTime),
+      }));
+    }
   };
 
   handleStopwatch = () => {
-    this.setState({
-      isRunning: !this.state.isRunning,
-    });
+    this.setState((prevState) => ({
+      isRunning: !prevState.isRunning,
+    }));
+    if (!this.state.isRunning) {
+      this.setState({ previousTime: Date.now() });
+    }
+  };
+
+  handleReset = () => {
+    this.setState({ elapsedTime: 0, isRunning: false });
   };
 
   render() {
+    const seconds = Math.floor(this.state.elapsedTime / 1000);
+
     return (
       <div className='stopwatch'>
         <h2>Stopwatch</h2>
-        <span className='stopwatch-time'>0</span>
+        <span className='stopwatch-time'>{seconds}</span>
         <button onClick={this.handleStopwatch}>
           {this.state.isRunning ? 'Stop' : 'Start'}
         </button>
-        <button>Reset</button>
+        <button onClick={this.handleReset}>Reset</button>
       </div>
     );
   }
